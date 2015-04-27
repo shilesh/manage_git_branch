7513fb347bab1373a7b5a453fd86519547436748
Shilesh Kumar <shilesh.kumar@v2solutions.com>
2015-04-27 12:14:25 +0530

    Adding validations in tests

--- test/options.sh
+++ test/options.sh
@@ -1,19 +1,29 @@
 function usage
 {
     echo "Usage:"
-    echo "./run.sh -n <issue number> -d <issue description> -m <update commit message template? (0/1)> -b <base branch for the new branch>"
+    echo "./run.sh -n <issue_number> -d <issue_description> -m <update commit message template? (0/1)> -b <base branch for the new branch>"
     exit
 }
+
+
 
 while [ "$1" != "" ]; do
     case $1 in
         -n | --issue_number )
             echo " issue_number:"$2
-                                ISSUE_NO=$2
-                                ;;
+            if [ "$2" == "" ];then
+                echo ' Issue number is mandatory'
+                usage
+            fi 
+            ISSUE_NO=$2
+            ;;
         -d | --issue_description )
-                                ISSUE_DESC=$1
-                                ;;
+            if [ "$1" == "" ];then
+                echo ' Issue description is mandatory'
+                usage
+            fi
+            ISSUE_DESC=$1
+            ;;
         -u | --update_message )
                                 EDIT_COMMIT_MSG=$1
                                 ;;
@@ -30,4 +40,14 @@ while [ "$1" != "" ]; do
     shift
 done
 
+# Validating mandatory fields
+
+if [ "$ISSUE_DESC" == "" ];then
+    echo ' Issue number is mandatory'
+    usage
+fi 
+if [ "$ISSUE_NO" == "" ];then
+    echo ' Issue number is mandatory'
+    usage
+fi 
 echo 'Parameters received :' $ISSUE_NO, $ISSUE_DESC, $EDIT_COMMIT_MSG, $ROOT_BRANCH
